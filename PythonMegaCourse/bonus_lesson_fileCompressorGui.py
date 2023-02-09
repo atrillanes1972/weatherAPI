932087f328df80e2210216801c12f3fedf0a7aa3
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-10 04:34:13 +0800

    Add bonus lesson File Compressor GUI app, now with Compress button.

--- PythonMegaCourse/bonus_lesson_fileCompressorGui.py
+++ PythonMegaCourse/bonus_lesson_fileCompressorGui.py
@@ -7,10 +7,12 @@ choose_button1 = sg.FilesBrowse("Choose")
 label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose")
+compress_button = sg.Button("Compress")
 
 window = sg.Window("File Compressor",layout=[
     [label1,input1,choose_button1],
-    [label2,input2,choose_button2]
+    [label2,input2,choose_button2],
+    [compress_button]
 ])
 
 window.read()
