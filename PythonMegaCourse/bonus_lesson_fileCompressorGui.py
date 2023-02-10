c9315a55447b0b2b4c68808594d56c0b4acf390a
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-11 04:45:09 +0800

    Done File Compressor app project, with status!

--- PythonMegaCourse/bonus_lesson_fileCompressorGui.py
+++ PythonMegaCourse/bonus_lesson_fileCompressorGui.py
@@ -9,11 +9,12 @@ label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose",key="folder")
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output",text_color="red")
 
 window = sg.Window("File Compressor",layout=[
     [label1,input1,choose_button1],
     [label2,input2,choose_button2],
-    [compress_button]
+    [compress_button,output_label]
 ])
 while True:
     event,values = window.read()
@@ -21,5 +22,6 @@ while True:
     filepaths = values["files"].split(";")
     folder = values["folder"]
     make_archive(filepaths,folder)
+    window["output"].update(value="Compression completed!")
 
 window.close()
