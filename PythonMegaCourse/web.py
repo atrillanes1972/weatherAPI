234c2eb53d6eef53c85ab4e5dfb182012c59fa54
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-16 03:51:53 +0800

    Updated Implement Add to-do items!

--- PythonMegaCourse/web.py
+++ PythonMegaCourse/web.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["newtodo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 
 st.title("My Todo App")
 st.subheader("This is my to-do app!")
@@ -10,4 +16,7 @@ st.write("This is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="newtodo")
+
+st.session_state
