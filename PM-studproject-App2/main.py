770163c778d3aabe0999eae0a0260796a00b6841
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-18 03:40:19 +0800

    Corrections to spacing and capitalizations.

--- PM-studproject-App2/main.py
+++ PM-studproject-App2/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+#Set page layout
+st.set_page_config(layout="wide")
 
 content="""
 Theres some latin description here on the company catalog below, and then the 
@@ -12,23 +14,23 @@ st.write(content)
 st.subheader("Our team")
 
 df = pd.read_csv("data.csv")
-col1, empty_col1,col2,empty_col2,col3 = st.columns([1.5,0.5,1.5,0.5,1.5])
+col1,col2,col3 = st.columns(3)
 
 with col1:
     for index,row in df[:4].iterrows():
-        st.header(row["first name"] + " " + row["last name"])
+        st.subheader(row["first name"].capitalize() + " " + row["last name"].capitalize())
         st.write(row["role"])
         st.image("images/" + row["image"])
 
 with col2:
     for index,row in df[4:8].iterrows():
-        st.header(row["first name"] + " " + row["last name"])
+        st.subheader(row["first name"].capitalize() + " " + row["last name"].capitalize())
         st.write(row["role"])
         st.image("images/" + row["image"])
 
 with col3:
     for index,row in df[8:12].iterrows():
-        st.header(row["first name"] + " " + row["last name"])
+        st.subheader(row["first name"].capitalize() + " " + row["last name"].capitalize())
         st.write(row["role"])
         st.image("images/" + row["image"])
 
