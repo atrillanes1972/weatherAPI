a117843cc64aa43c1ade2026701f4236ee094c76
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-18 05:46:05 +0800

    Added another fix for the Email subject line.

--- PM-studproject-App2/Contact_Us.py
+++ PM-studproject-App2/Contact_Us.py
@@ -1,11 +1,13 @@
 import streamlit as st
+from sendemail import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms")
     user_email = st.text_input("Your email address")
     message = st.text_area("Your message")
+    message = message + "\n" + user_email
     button = st.form_submit_button("Submit")
     print(button)
     if button:
-        print("Pressed!")
+        send_email(message)
--- PM-studproject-App2/sendemail.py
+++ PM-studproject-App2/sendemail.py
@@ -1,20 +1,15 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "curzekonrad72@gmail.com"
-password = "cpoiufulxpkfztnx"
+    username = "curzekonrad72@gmail.com"
+    password = "cpoiufulxpkfztnx"
 
-receiver = "hlupercal40k@gmail.com"
-context = ssl.create_default_context()
+    receiver = "hlupercal40k@gmail.com"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Testing Email!
-Hi, how are you!
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username,password)
-    server.sendmail(username, receiver,message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username,password)
+        server.sendmail(username, receiver,message)
