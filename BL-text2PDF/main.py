2d05ec8028bffb0d4cda4ec190fcd2dca4fdfe84
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-20 03:43:28 +0800

    Fixes in the filename.

--- BL-text2PDF/main.py
+++ BL-text2PDF/main.py
@@ -4,13 +4,15 @@ import glob
 
 filepaths = glob.glob(r'texts\*.txt')
 
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
 for filepath in filepaths:
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    filename = Path(filepath).stem.capitalize()
+    filename = Path(filepath).stem
+    name = filename.capitalize()
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=10, txt=f"{filename}")
+    pdf.cell(w=50, h=10, txt=f"{name}")
     pdf.output(f"PDFs\{filename}.pdf")
 
 
