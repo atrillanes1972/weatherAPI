2ee7275085cdebdf7b349b3e5aa0c1a06dc30863
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-20 06:59:47 +0800

    Fixes in the filename. Plus it's now on one file output.

--- BL-text2PDF/main.py
+++ BL-text2PDF/main.py
@@ -8,11 +8,16 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
     pdf.add_page()
+
     filename = Path(filepath).stem
     name = filename.capitalize()
-
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=10, txt=f"{name}")
-    pdf.output(f"PDFs\{filename}.pdf")
+    pdf.cell(w=50, h=8, txt=name, ln=1)
 
+    with open(filepath, "r") as file:
+        content= file.read()
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+pdf.output("output.pdf")
 
