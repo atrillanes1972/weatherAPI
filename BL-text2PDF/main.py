0b2157d698fa036199685bf88094fe9c31b2be1a
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-02-20 03:34:03 +0800

    Initial Commit. Created simple pdfs with animal names on the titles each.

--- BL-text2PDF/main.py
+++ BL-text2PDF/main.py
@@ -8,7 +8,7 @@ for filepath in filepaths:
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     filename = Path(filepath).stem.capitalize()
-    #print(filename)
+
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=10, txt=f"{filename}")
     pdf.output(f"PDFs\{filename}.pdf")
